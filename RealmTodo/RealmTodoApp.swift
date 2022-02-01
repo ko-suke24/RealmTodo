d6dc8f657ebca1b9b720274e6035e906df1511ed
ko-suke24 <kosuke0322@gmail.com>
2022-02-01 17:20:57 +0900

    Initial Commit

---
+++ RealmTodo/RealmTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmTodoApp.swift
+//  RealmTodo
+//
+//  Created by 佐藤航輔 on 2022/02/01.
+//
+
+import SwiftUI
+
+@main
+struct RealmTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
